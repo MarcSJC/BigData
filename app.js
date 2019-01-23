df33d29a235f08c3f935ed1308c020b37d94659d
Marc Saint-Jean-Clergeau <mcsjean@emi.u-bordeaux1.fr>
2019-01-23 15:43:08 +0100

    +

--- app.js
+++ app.js
@@ -2,22 +2,30 @@
 const express = require('express');
 const app = express();
 const fs = require('file-system');
-const nopath = "C:/Users/onewe/Desktop/raw/testtiles/no.png";
-
+const assert = require('assert');
+const hbase = require('hbase');
+const nopath = __dirname + "/no.png";
+//8/381/92
 app.get('/tile/:z/:x/:y', function (req, res) {
 	console.log(req.path);
 	let px = parseInt(req.params.x);
 	let py = parseInt(req.params.y);
 	let zoom = parseInt(req.params.z);
 	let path = zoom + "/" + px + "/" + py;
-	if (fs.existsSync(path)) {
+	hbase({ host: 'young', port: 9000 })
+	.table('PascalTestTiles')
+	.row(path)	
+	.get('File:Tile', (error, value) => {
+		console.info(value);
+	});
+	/*if (fs.existsSync(path)) {
 		console.log(path);
 		res.sendFile(path);
 	}
 	else {
 		console.log(nopath);
 		res.sendFile(nopath);
-	}
+	}*/
 })
 
 app.listen(3000, function () {
