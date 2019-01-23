a880a45ec5a2d8dab6ab31a346f44c68e80ed00c
MarcSJC <26414156+MarcSJC@users.noreply.github.com>
2019-01-23 13:35:25 +0100

    before hbase

--- app.js
+++ app.js
@@ -7,13 +7,9 @@ const nopath = "C:/Users/onewe/Desktop/raw/testtiles/no.png";
 app.get('/tile/:z/:x/:y', function (req, res) {
 	console.log(req.path);
 	let px = parseInt(req.params.x);
-	//let xtile = Math.floor(px / 2);
-	let py = parseInt(req.params.y/*.substring(0, req.params.y.length - 4)*/);
-	//console.log(req.params.y);
-	//let ytile = Math.floor(py / 2);
+	let py = parseInt(req.params.y);
 	let zoom = parseInt(req.params.z);
-	//let path = "C:/Users/onewe/Desktop/raw/testtiles/" + zoom + "/" + xtile + "/" + ytile + ".png";
-	let path = "C:/Users/onewe/Desktop/raw/testtiles/" + zoom + "/" + px + "/" + py + ".png";
+	let path = zoom + "/" + px + "/" + py;
 	if (fs.existsSync(path)) {
 		console.log(path);
 		res.sendFile(path);
